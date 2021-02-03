9a780db94a2c4296ff4a438f9af21233d0b14e47
ShivaniJ-hub <71375978+ShivaniJ-hub@users.noreply.github.com>
2021-02-03 21:07:28 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.11.0"
+    }
+  }
+}
+provider "docker" {
+
+  registry_auth {
+    address = "registry.hub.docker.com"
+    username = "shivani221"
+    password = var.password
+  }
+}
+
+resource "docker_image" "myimage" {
+  name = "shivani221/tomcatserver:latest"
+  build {
+      path="."
+  }
+}
