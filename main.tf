990cd3d836b5685d72be3878ec618bc657fe799f
ShivaniJ-hub <71375978+ShivaniJ-hub@users.noreply.github.com>
2021-02-04 12:51:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,6 +32,39 @@ resource "docker_container" "mycontainer" {
     external = 9090
   }
 }
+resource "docker_network" "my_network" {
+  name = "selenium"
+}
+
+resource "docker_image" "selenium" {
+  name = "selenium/hub"
+}
+resource "docker_container" "selenium-hub" {
+  name = "selenium-hub"
+  image = docker_image.selenium.latest
+  ports {
+    internal = 4444
+    external = 4444
+  }
+  env = toset(["GRID_TIMEOUT=240000","GRID_BROWSER_TIMEOUT=240000"])
+  networks_advanced {
+	name = docker_network.my_network.name
+  }
+}
+resource "docker_image" "chromeimg" {
+  name = "selenium/node-chrome"
+}
+resource "docker_container" "chrome_1" {
+  name  = "chrome"
+  image = docker_image.chromeimg.latest
+  ports {
+    internal = 5900
+  }
+  env = toset(["HUB_HOST=selenium-hub","HUB_PORT=4444","JAVA_OPTS=-Dwebdriver.chrome.whitelistedIps="])
+  networks_advanced {
+	name = docker_network.my_network.name
+  }
+}
 
 variable "password" {
   type = string
