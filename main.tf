1d2031ba2f99fab0d7b8f3a82aff4becaf1d18f1
ShivaniJ-hub <71375978+ShivaniJ-hub@users.noreply.github.com>
2021-02-08 18:29:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,23 @@ module "tomcat_container" {
   password=var.pass
 }
 
- module "testing_containers" {
+module "testing_containers" {
   source = "./testing"
+} 
+   
+module "awstomcat" {
+  source = "./awstomcat"
 } 
   
 variable "pass" {
   type = string
+  default = "password"
+}
+variable "acc" {
+  type = string
+  default = "access-key"
+}
+variable "sec" {
+  type = string
+  default = "secret-key"
 }
