0261c36d51d773b9b8b8d413f744592e824fa1ab
ShivaniJ-hub <71375978+ShivaniJ-hub@users.noreply.github.com>
2021-02-09 13:06:13 +0530

    Update awstomcat.tf

--- awstomcat/awstomcat.tf
+++ awstomcat/awstomcat.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "Tomcat-Server" {
     }
 }
 output "aws_link" {
-  value=format("Access the AWS hosted app from here: %s%s", aws_instance.Tomcat-Server.public_dns, ":8080/MusicStore")
+  value=format("Access the AWS hosted app from here: http://%s%s", aws_instance.Tomcat-Server.public_dns, ":8080/MusicStore")
 }
 data "template_file" "asg_init" {
   template = file("${path.module}/userdata.tpl")
